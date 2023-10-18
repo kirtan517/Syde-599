3f5d4f2ee7f85f219133b9324a95e192a7a76ef1
kirtan517 <kananikirtan73@gmail.com>
2023-10-18 18:18:31 -0400

    added

--- Assignment-1/torch_model.py
+++ Assignment-1/torch_model.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch
 import pickle
+import torch.optim as optim
 
 class SimpleNN(nn.Module):
     def __init__(self):
@@ -31,7 +32,27 @@ model.fc3.weight.data = torch.tensor(weights_and_biases['w3']).float()
 model.fc3.bias.data = torch.tensor(weights_and_biases['b3']).float()
 
 input_data = torch.tensor(weights_and_biases['inputs']).float()
-targets = torch.tensor(weights_and_biases['targets']).float()
+targets = torch.tensor(weights_and_biases['targets']).float().unsqueeze(1)
+# targets = torch.reshape(targets,(200,1))
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+loss_function = nn.MSELoss(reduction="sum")
+print(input_data.shape)
+print(targets.shape)
+
+
+n_epochs = 10
+for i in range(n_epochs):
+    output = model(input_data)
+    loss = loss_function(output,targets)
+    loss.backward()
+    print(loss)
+    optimizer.step()
+    optimizer.zero_grad()
+
+
 
 predictions = model(input_data)
 print(predictions)
+
+#%%
