b2cc1446f5890e2c63ca2750a9023e0cd921e040
kirtan517 <kananikirtan73@gmail.com>
2023-10-18 21:16:50 -0400

    Working Similar to pytorch

--- Assignment-1/torch_model.py
+++ Assignment-1/torch_model.py
@@ -33,11 +33,11 @@ model.fc3.weight.data = torch.tensor(weights_and_biases['w3']).float()
 model.fc3.bias.data = torch.tensor(weights_and_biases['b3']).float()
 
 input_data = torch.tensor(weights_and_biases['inputs']).float()
-targets = torch.tensor(weights_and_biases['targets']).float().unsqueeze(1).unsqueeze(1)
+targets = torch.tensor(weights_and_biases['targets']).float().unsqueeze(1)
 # targets = torch.reshape(targets,(200,1))
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-loss_function = nn.MSELoss(reduction="sum")
+loss_function = nn.MSELoss(reduction="mean")
 print(input_data.shape)
 print(targets.shape)
 
@@ -57,43 +57,43 @@ predictions = model(input_data)
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-n_epochs = 200    # number of epochs to run
-batch_size = 10  # size of each batch
-batches_per_epoch = len(input_data) // batch_size
-
-loss = loss_fn(predictions, targets)
-loss.backward()
-print(f"Pre-train loss: {loss/2}")
-
-loss_history = []
-
-for epoch in range(n_epochs):
-    for i in range(batches_per_epoch):
-        start = i * batch_size
-        # take a batch
-        Xbatch = input_data[start:start+batch_size]
-        ybatch = targets[start:start+batch_size]
-        # forward pass
-        y_pred = model(Xbatch)
-        loss = loss_fn(y_pred, ybatch)
-        # backward pass
-        optimizer.zero_grad()
-        loss.backward()
-        # update weights
-        optimizer.step()
-    loss_history.append(loss)
-
-# Create a figure and axis
-plt.figure(figsize=(10,6))
-plt.plot([l.detach().numpy() for l in loss_history])
-
-# Add titles and labels
-plt.title('Training Loss Over Time')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.grid(True)
-
-# Show the plot
-plt.show()
-
-print(f"After training loss: {loss/2}")
+# n_epochs = 200    # number of epochs to run
+# batch_size = 10  # size of each batch
+# batches_per_epoch = len(input_data) // batch_size
+#
+# loss = loss_fn(predictions, targets)
+# loss.backward()
+# print(f"Pre-train loss: {loss/2}")
+#
+# loss_history = []
+#
+# for epoch in range(n_epochs):
+#     for i in range(batches_per_epoch):
+#         start = i * batch_size
+#         # take a batch
+#         Xbatch = input_data[start:start+batch_size]
+#         ybatch = targets[start:start+batch_size]
+#         # forward pass
+#         y_pred = model(Xbatch)
+#         loss = loss_fn(y_pred, ybatch)
+#         # backward pass
+#         optimizer.zero_grad()
+#         loss.backward()
+#         # update weights
+#         optimizer.step()
+#     loss_history.append(loss)
+#
+# # Create a figure and axis
+# plt.figure(figsize=(10,6))
+# plt.plot([l.detach().numpy() for l in loss_history])
+#
+# # Add titles and labels
+# plt.title('Training Loss Over Time')
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.grid(True)
+#
+# # Show the plot
+# plt.show()
+#
+# print(f"After training loss: {loss/2}")
